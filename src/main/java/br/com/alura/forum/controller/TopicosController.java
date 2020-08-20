f645e42f9357b37a545eaf087a6ec126fa1f087c
Mateus <mateus.fmaciel@uni9.edu.br>
2020-08-19 23:15:23 -0300

    filtro por data

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,6 +1,10 @@
 package br.com.alura.forum.controller;
 
+import java.io.Console;
 import java.net.URI;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -16,6 +20,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
@@ -33,70 +38,76 @@ public class TopicosController {
 
 	@Autowired
 	private TopicoRepository topicoRepository;
-	
+
 	@Autowired
 	private CursoRepository cursoRepository;
-	
-	@GetMapping
-	public List<TopicoDTO> lista(String nomeCurso){
-		if(nomeCurso == null) {
+
+	@RequestMapping("/todos")
+	public List<TopicoDTO> lista(String nomeCurso) {
+		if (nomeCurso == null) {
 			List<Topico> topicos = topicoRepository.findAll();
 			return TopicoDTO.converter(topicos);
-		}
-		else {
+		} else {
 			List<Topico> topicos = topicoRepository.findByCursoNome(nomeCurso);
 			return TopicoDTO.converter(topicos);
-		}		
+		}
 	}
-	
+
 	@PostMapping
 	@Transactional
 	public ResponseEntity<TopicoDTO> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder) {
-		
+
 		Topico topico = form.converter(cursoRepository);
 		topicoRepository.save(topico);
-		
+
 		URI uri = uriBuilder.path("/topicos/{id}").buildAndExpand(topico.getId()).toUri();
 		return ResponseEntity.created(uri).body(new TopicoDTO(topico));
 	}
-	
-	@GetMapping("/{id}")
-	public ResponseEntity<DetalhesDoTopicoDTO> detalhar(@PathVariable Long id) {
-		Optional<Topico> topico = topicoRepository.findById(id);
+
+	@GetMapping
+	public List<Topico> detalhar(@RequestParam String id, @RequestParam String data_ini, @RequestParam String data_fim) {
 		
-		if(topico.isPresent()) {
-			return ResponseEntity.ok(new DetalhesDoTopicoDTO(topico.get()));	
+		List<Topico> topicos = topicoRepository.findAll();
+		
+		LocalDateTime dataIni = LocalDateTime.parse(data_ini);
+		LocalDateTime dataFim = LocalDateTime.parse(data_fim);
+		
+		
+		List<Topico> topicosFiltrados = new ArrayList<Topico>();
+		for(Topico topico : topicos) {
+			if(topico.getDataCriacao().compareTo(dataIni) >= 0 && topico.getDataCriacao().compareTo(dataFim) <= 0) {
+				topicosFiltrados.add(topico);
+			}
 		}
 		
-		return ResponseEntity.notFound().build();
-		
+		return topicosFiltrados;		
 	}
-	
+
 	@PutMapping("/{id}")
 	@Transactional
-	public ResponseEntity<TopicoDTO> atualizar(@PathVariable Long id,@RequestBody @Valid AtualizacaoTopicoForm form){
-		
+	public ResponseEntity<TopicoDTO> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form) {
+
 		Optional<Topico> optional = topicoRepository.findById(id);
-		
-		if(optional.isPresent()) {
+
+		if (optional.isPresent()) {
 			Topico topico = form.atualizar(id, topicoRepository);
-			return ResponseEntity.ok(new TopicoDTO(topico));	
+			return ResponseEntity.ok(new TopicoDTO(topico));
 		}
-		
-		return ResponseEntity.notFound().build();			
+
+		return ResponseEntity.notFound().build();
 	}
-	
+
 	@DeleteMapping("/{id}")
 	@Transactional
-	public ResponseEntity<?> remover(@PathVariable Long id){
-		
+	public ResponseEntity<?> remover(@PathVariable Long id) {
+
 		Optional<Topico> optional = topicoRepository.findById(id);
-		
-		if(optional.isPresent()) {
+
+		if (optional.isPresent()) {
 			topicoRepository.deleteById(id);
-			return ResponseEntity.ok().build();				
+			return ResponseEntity.ok().build();
 		}
-		
+
 		return ResponseEntity.notFound().build();
 	}
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,4 +5,16 @@ INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
 
 INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida', 'Erro ao criar projeto', '2019-05-05 18:00:00', 'NAO_RESPONDIDO', 1, 1);
 INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 2', 'Projeto não compila', '2019-05-05 19:00:00', 'NAO_RESPONDIDO', 1, 1);
-INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 3', 'Tag HTML', '2019-05-05 20:00:00', 'NAO_RESPONDIDO', 1, 2);
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 3', 'Tag HTML1', '2015-05-05 20:00:00', 'NAO_RESPONDIDO', 1, 2);
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 4', 'Tag HTML2', '2016-05-05 20:00:00', 'NAO_RESPONDIDO', 1, 2);
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 5', 'Tag HTML3', '2017-05-05 20:00:00', 'NAO_RESPONDIDO', 1, 2);
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 6', 'Tag HTML4', '2018-05-05 20:00:00', 'NAO_RESPONDIDO', 1, 2);
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 7', 'Tag HTML5', '2019-05-05 20:00:00', 'NAO_RESPONDIDO', 1, 2);
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 8', 'Tag HTML6', '2020-05-05 20:00:00', 'NAO_RESPONDIDO', 1, 2);
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 9', 'Tag HTML7', '2020-06-05 20:00:00', 'NAO_RESPONDIDO', 1, 2);
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 10', 'Tag HTML8', '2020-06-06 20:00:00', 'NAO_RESPONDIDO', 1, 2);
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 11', 'Tag HTML9', '2020-06-07 20:00:00', 'NAO_RESPONDIDO', 1, 2);
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 12', 'Tag HTML10', '2020-06-08 20:00:00', 'NAO_RESPONDIDO', 1, 2);
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 13', 'Tag HTML11', '2020-06-09 20:00:00', 'NAO_RESPONDIDO', 1, 2);
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 14', 'Tag HTML12', '2021-05-05 20:00:00', 'NAO_RESPONDIDO', 1, 2);
+INSERT INTO TOPICO(titulo, mensagem, data_criacao, status, autor_id, curso_id) VALUES('Dúvida 15', 'Tag HTML13', '2022-05-05 20:00:00', 'NAO_RESPONDIDO', 1, 2);
